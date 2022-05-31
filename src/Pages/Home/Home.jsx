f03697c6f54036fb6b7f8fd7fa86fdf0db85470a
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-05-31 17:14:51 +0100

    added new features

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,10 @@
 import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
+import MailList from "../../components/mailList/MailList";
+import PropertyList from "../../components/propertyList/PropertyList";
+import Footer from "../../components/footer/Footer";
 import "./Home.css";
 
 function Home() {
@@ -12,6 +16,11 @@ function Home() {
       <div className="homeContainer">
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guest love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
       </div>
     </>
   );
